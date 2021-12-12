2cb91a34d904a3c90432a2fe85a90f84555ae676
Artem Denysov <denysov.artem@gmail.com>
2021-12-12 22:05:46 +0200

    improve api

--- index.js
+++ index.js
@@ -2,45 +2,54 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const axios = require("axios");
 
-const waitForUrl = async (url, MAX_TIMEOUT, { headers }) => {
-  const iterations = MAX_TIMEOUT / 2;
-  for (let i = 0; i < iterations; i++) {
-    try {
-      await axios.get(url, { headers });
-      return;
-    } catch (e) {
-      console.log("Url unavailable, retrying...");
-      await new Promise((r) => setTimeout(r, 2000));
-    }
-  }
-  core.setFailed(`Timeout reached: Unable to connect to ${url}`);
+const apiGet = async ({ data, path }) => {
+	const apiUrl = 'https://api.netlify.com/api/v1';
+	const response = await axios.get(`${apiUrl}/${path}`, {
+		body: JSON.stringify(data),
+		headers: getHeaders({ token: api.accessToken })
+	});
+
+	await checkResponse({ response });
+
+	return response;
+}
+
+const waitForLive = async ({ siteId, sha, MAX_TIMEOUT }) => {
+	const iterations = MAX_TIMEOUT / 2;
+	for (let i = 0; i < iterations; i++) {
+		try {
+			const deploys = await apiGet({ path: `/sites/${siteId}/deploys` });
+			const currentDeploy = deploys.find({ commit_ref: sha });
+			if (currentDeploy && ['production', 'deploy_preview'].includes(currentDeploy.context)) {
+				if (currentDeploy.state === 'ready') {
+					return true;
+				} else if (currentDeploy.state === 'failed') {
+					core.setFailed("Netlify deploy failed");
+					return false;
+				}
+			} else {
+				await new Promise((r) => setTimeout(r, 2000));
+			}
+		} catch (e) {
+			core.setFailed("Wait for Netlify failed");
+		}
+	}
 };
 
 const run = async () => {
-  try {
-    const PR_NUMBER = github.context.payload.number;
-    if (!PR_NUMBER) {
-      core.setFailed(
-        "Action must be run in conjunction with the `pull_request` event"
-      );
-    }
-    const MAX_TIMEOUT = Number(core.getInput("max_timeout")) || 60;
-    const siteName = core.getInput("site_name");
-    const basePath = core.getInput("base_path");
-    if (!siteName) {
-      core.setFailed("Required field `site_name` was not provided");
-    }
-    const url = `https://deploy-preview-${PR_NUMBER}--${siteName}.netlify.app${basePath}`;
-    core.setOutput("url", url);
-    const extraHeaders = core.getInput("request_headers");
-    const headers = !extraHeaders ? {} : JSON.parse(extraHeaders)
-    console.log(`Waiting for a 200 from: ${url}`);
-    await waitForUrl(url, MAX_TIMEOUT, {
-      headers,
-    });
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+	try {
+		const siteId = core.getInput("site_id");
+		const MAX_TIMEOUT = Number(core.getInput("max_timeout")) || 60;
+
+		if (!siteId) {
+			core.setFailed("Required field `siteId` was not provided");
+		}
+
+		return await waitForLive({ MAX_TIMEOUT, siteId, sha: github.context.payload.head_commit });
+
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 };
 
 run();
